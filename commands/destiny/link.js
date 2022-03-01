b13ea3fbc6243cf77db5958374b9d2f630ad33cd
Thomas STROOBANTS <stroobants.thomas@gmail.com>
2022-03-01 12:29:18 +0100

    Link command added

--- commands/destiny/link.js
+++ commands/destiny/link.js
@@ -30,26 +30,57 @@ module.exports = {
 
         if(guildData.isExistingLinkedUser(discordID)) {
             inf('This user is already registered.');
-            await interaction.reply({content: `Your account is already linked : ${guildData.getLinkedUser(discordID).UniqueBungieName}`, ephemeral: true});
+            await interaction.reply({content: `Your account is already linked : ${guildData.getLinkedUser(discordID)}`, ephemeral: true});
             return;
         }
 
         bungieAPI = new BungieAPI();
         bungieAPI.get(`/Destiny2/SearchDestinyPlayer/-1/${bungieTag}/`)
-            .then(res => {
-                //console.log(`statusCode: ${res.status}`)
+            .then(async res => {
+                inf(`Status code: ${res.status}`);
                 //console.log(res.data.Response)
                 if(bungieAPI.isBungieAPIDown(res)) {
-                    await interaction.reply('Destiny 2 API is down, it might be a maintenance ongoing. Check again later.');
+                    inf('Bungie API down.')
+                    await interaction.reply({content: 'Destiny 2 API is down, it might be a maintenance ongoing. Check again later.', ephemeral: true});
                     return;
                 };
-              })
+
+                if(res.data.Response[0]==undefined) {
+                    inf('Wrong Bungie Tag')
+                    await interaction.reply({content: 'No Bungie profile found. Are your sure about your Bungie tag ?', ephemeral: true});
+                    return;
+                }
+
+                memberID = res.data.Response[0].membershipId;
+                if(res.data.Response[0].crossSaveOverride==0) {
+                    memberType = res.data.Response[0].membershipType;
+                } else {
+                    for (type of res.data.Response) {
+                        if (type.crossSaveOverride==type.membershipType) {
+                            memberType = type.membershipType;
+                            break;
+                        };
+                    };
+                };
+
+                memberPlateform = bungieAPI.getPlateform(memberType);
+
+                if(!bungieAPI.isPublicAccount(res.data.Response)) {
+                    await interaction.reply({content: 'Your account is set on private, we we won\'t be able to access your informations.', ephemeral: true});
+                }
+
+                dbg(`memberID: ${memberID}`);
+                dbg(`memberType: ${memberType}`);
+                dbg(`memberPlateform: ${memberPlateform}`);
+
+                guildData.addUser(discordID, memberID, memberType, bungieTag);
+                guildData.getUsersList();
+
+                await interaction.reply('You linked your account.');
+
+            })
             .catch(error => {
                 console.error(error)
-            })
-        
-        //{ memID, memType } = guildData.GetValidDestinyMembership();
-
-        await interaction.reply('You linked your account.');
+            });
     },
 };
